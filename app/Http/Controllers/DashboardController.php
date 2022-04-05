266bc8a20b079e20cdb8ae2c0ffd6fa035aa7303
bolivar014 <bolivar014@gmail.com>
2022-04-05 18:05:37 -0500

    Request

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,9 +7,11 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     // Función para el index del dashboard
-    public function index() {
+    public function index(Request $request) {
+        // En caso que no reciba un title, se asigna un valor por defecto
+
         return view('/test', [
-            'title' => 'Curso de platzi'
+            'title' => $request->query('title', 'Valor default')
         ]);
     }
 }
