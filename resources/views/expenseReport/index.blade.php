f6817e65a7cd535d278006ef32229834677c0f96
bolivar014 <bolivar014@gmail.com>
2022-04-06 09:23:36 -0500

    formulario para agregar reportes

--- resources/views/expenseReport/index.blade.php
+++ resources/views/expenseReport/index.blade.php
@@ -4,6 +4,12 @@
             <div class="row">
                 <div class="col">
                     <h1>Reports</h1>
+                    <a class="btn btn-primary" href="/expense_reports/create">Create a New Report</a>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col">
                     <table class="table">
                         @foreach ($expenseReports as $expenseReport)
                             <tr>
